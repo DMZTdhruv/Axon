d860ca8a517eded10cd49664e03bd00abd728c89
DMZTdhruv <shruvparmar.gmail.com>
2024-09-27 19:38:19 +0530

    [BUG FIXED] -> menu item links are highligthed correctly

--- axon_client/src/components/navbar/navlink.tsx
+++ axon_client/src/components/navbar/navlink.tsx
@@ -5,18 +5,24 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 const NavLink = ({ link }: { link: TMenuItem }) => {
-  const router = usePathname();
-  const isActive = router.includes(link.url);
-  return (
-    <Link
-      href={`/${link.url}`}
-      key={link.title}
-      className={`flex w-fit text-[13px] ${isActive ? "opacity-100" : "opacity-60 "} hover:opacity-100 transition-all  gap-[10px]`}
-    >
-      <img width={17} height={17} src={`/assets/${link.icon}`} alt={`icon_${link.title}`} />
-      <span>{link.title}</span>
-    </Link>
-  );
+	const router = usePathname();
+	const isActive = router === `/${link.url}`;
+
+	return (
+		<Link
+			href={`/${link.url}`}
+			key={link.title}
+			className={`flex w-fit text-[13px] ${isActive ? "opacity-100" : "opacity-60 "} hover:opacity-100 transition-all  gap-[10px]`}
+		>
+			<img
+				width={17}
+				height={17}
+				src={`/assets/${link.icon}`}
+				alt={`icon_${link.title}`}
+			/>
+			<span>{link.title}</span>
+		</Link>
+	);
 };
 
 export default NavLink;

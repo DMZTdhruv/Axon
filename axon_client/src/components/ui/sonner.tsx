6f1868adccf2ed02cc731f03c5070598e4c85d84
DMZTdhruv <shruvparmar.gmail.com>
2024-10-03 12:27:12 +0530

    Improved toast ui

--- axon_client/src/components/ui/sonner.tsx
+++ axon_client/src/components/ui/sonner.tsx
@@ -15,7 +15,7 @@ const Toaster = ({ ...props }: ToasterProps) => {
 			toastOptions={{
 				classNames: {
 					toast:
-						"group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg",
+						"group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg cursor-pointer",
 					description: "group-[.toast]:text-muted-foreground",
 					actionButton:
 						"group-[.toast]:bg-primary font-bold group-[.toast]:text-primary-foreground",
